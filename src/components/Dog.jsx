f27f8ced33ad5d1a6a0e6a1481bbfce22d863435
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-17 19:33:02 -0300

    Criando botao para adicionar mais uma imagem na tela e mensagem de loading para cada requisiao

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -5,32 +5,73 @@ class Dog extends React.Component {
     super();
 
     this.fetchDog = this.fetchDog.bind(this);
+    this.renderDogImage = this.renderDogImage.bind(this);
+    this.oneMore = this.oneMore.bind(this);
 
     this.state = {
       imageDog: '',
       loading: true,
+      allDogs: [],
     };
   }
 
   fetchDog() {
-    const url = 'https://dog.ceo/api/breeds/image/random';
-    fetch(url)
-      .then((response) => response.json())
-      .then((response) =>
-        this.setState({
-          imageDog: response.message,
-        }),
-      );
+    this.setState(
+      {
+        loading: true,
+      },
+      () => {
+        const url = 'https://dog.ceo/api/breeds/image/random';
+        fetch(url)
+          .then((response) => response.json())
+          .then((response) =>
+            this.setState({
+              loading: false,
+              imageDog: response.message,
+            }),
+          );
+      },
+    );
   }
 
   componentDidMount() {
     this.fetchDog();
   }
 
+  oneMore() {
+    this.setState((previousState) => ({
+      allDogs: [...previousState.allDogs, previousState.imageDog],
+    }));
+
+    this.fetchDog();
+  }
+
+  renderDogImage() {
+    return (
+      <div>
+        <figure>
+          <img src={this.state.imageDog} alt="random-dog" className="image" />
+        </figure>
+
+        <button type="button" onClick={this.oneMore}>
+          Mais um!
+        </button>
+      </div>
+    );
+  }
+
   render() {
+    const loadingElement = <span>Loading...</span>;
+
     return (
       <div className="dog-conteiner">
-        <img src={this.state.imageDog} alt="random-dog"></img>
+        <figure>
+          {this.state.allDogs.map((element) => (
+            <img src={element} alt="random-dog" className="image" />
+          ))}
+        </figure>
+
+        {this.state.loading ? loadingElement : this.renderDogImage()}
       </div>
     );
   }
